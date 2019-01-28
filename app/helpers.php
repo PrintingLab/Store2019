9e06b046b2fdf14da92f91b95496e5387548ea7b
hlc <hernando.printinglab@gmail.com>
2019-01-28 09:55:08 -0500

    a

--- app/helpers.php
+++ app/helpers.php
@@ -1,218 +1,235 @@
-<?php
+  <?php
 
-use Carbon\Carbon;
-use App\Product;
+  use Carbon\Carbon;
+  use App\Product;
 
-function getAllProducts(){
-    $allproducts = Product::all();
-   
-    return $allproducts;
+  function getAllProducts(){
+      $allproducts = Product::all();
+
+      return $allproducts;
+    }
+
+
+  function presentPrice($price)
+  {
+      return '$'.number_format($price, 2);
+  }
+  function printingPrice($price)
+  {
+      $url = public_path()."/storage/jsonconfig/priceprintinglab.json";
+      $json = file_get_contents($url);
+      $json_data = json_decode($json, true);
+        foreach ($json_data as $value) {
+            if ($price >=$value['Pinicial'] && $price <=$value['Pfinal']) {
+                $newprice =($price*$value['porcentaje'])/100;
+               return  $newprice;
+            }
+
+       }
+
   }
 
-
-function presentPrice($price)
-{
-    return '$'.number_format($price, 2);
-}
-function printingPrice($price)
-{
-    $url = public_path()."/storage/jsonconfig/priceprintinglab.json";
-    $json = file_get_contents($url);
-    $json_data = json_decode($json, true);
-      foreach ($json_data as $value) {
-          if ($price >=$value['Pinicial'] && $price <=$value['Pfinal']) {
-              $newprice =($price*$value['porcentaje'])/100;
-             return  $newprice;
-          }
-
-     }
-
-}
-
-function printingtax($postalcode)
-{
-    $url = public_path()."/storage/jsonconfig/zips.json";
-    $json = file_get_contents($url);
-    $json_data = json_decode($json, true);
-      foreach ($json_data as $value) {
-         if ($postalcode == $value) {
-            $taxval  = 6.32;
-            break;
-         }else {
-            $taxval  = 0;
-         }
-     }
-return  $taxval;
-}
-
-function extensioImg($imagen){
-  $result=  ltrim(strstr($imagen, '.'), '.');
-  return($result);
-}
-
-
-function presentDate($date)
-{
-    return Carbon::parse($date)->format('M d, Y');
-}
-
-function setActiveCategory($category, $output = 'active')
-{
-    return request()->category == $category ? $output : '';
-}
-
-function productImage($path)
-{
-    return $path && file_exists('storage/'.$path) ? asset('storage/'.$path) : asset('img/not-found.jpg');
-}
-
-function getshipingNumbers()
-{
-    foreach (Cart::content() as $item){
-        $price =  $item->options->shiping;
-    }
-    if (isset($price)) {
-       return $price;
-    }
-
-}
-
-function getPrintingsides($prttime)
-{
-   switch ($prttime) {
-       case '4/0':
-           return 'Front Only';
-           break;
-       case '4/4':
-           return 'Front and Back';
-           break;
-       default:
-           'N/A';
-           break;
-   }
-}
-
-function getPrintingTime($prttime)
-{
-    $method = 'GET';
-    $separator ='?';
-    $json = '';
-    $uri='https://api.4over.com/printproducts/optiongroups/f80e8179-b264-42ce-9f80-bb258a09a1fe/options';
-    $result = json_decode(call4overcurl($uri,$method,$separator,$json), true);
-    foreach ($result['entities'] as $value) {
-        if ($value['option_name'] == $prttime) {
-           return $value['option_description'];
-          }
+  function printingtax($postalcode)
+  {
+      $url = public_path()."/storage/jsonconfig/zips.json";
+      $json = file_get_contents($url);
+      $json_data = json_decode($json, true);
+        foreach ($json_data as $value) {
+           if ($postalcode == $value) {
+              $taxval  = 6.32;
+              break;
+           }else {
+              $taxval  = 0;
+           }
        }
-}
+  return  $taxval;
+  }
 
-function getNumbers()
-{
-    //tax for zip funtion
-    $cookie_name="Zipcode";
-    if(!isset($_COOKIE[$cookie_name])) {
-        $tax = 0 / 100;
-   } else {
-    $tax = printingtax($_COOKIE[$cookie_name]) / 100;
-   }
-    $discount = session()->get('coupon')['discount'] ?? 0;
-    $code = session()->get('coupon')['name'] ?? null;
-    $newSubtotal = (Cart::subtotal() - $discount);
-    $shiping = getshipingNumbers();
-    if ($newSubtotal < 0) {
-        $newSubtotal = 0;
-    }
-    $newTax = $newSubtotal * $tax;
-    $newTotal = ($newSubtotal * (1 + $tax)) + getshipingNumbers();
+  function extensioImg($imagen){
+    $result=  ltrim(strstr($imagen, '.'), '.');
+    return($result);
+  }
 
-    return collect([
-        'tax' => $tax,
-        'discount' => $discount,
-        'code' => $code,
-        'newSubtotal' => $newSubtotal,
-        'newTax' => $newTax,
-        'newTotal' => $newTotal,
-        'shiping' => $shiping,
-    ]);
-}
-function get4overprices($UUID,$runsize,$colorspec,$TurnAroundTime)
-{
+  function productsTempleteEps($url){
+  $directory = array_diff(scandir($url), array('..', '.'));
+  $array_eps=array_diff(scandir($url.$directory[2]), array('..', '.'));
+  return($array_eps);
+  }
+
+  function productsTempleteJpg($url){
+  $directory = array_diff(scandir($url), array('..', '.'));
+  $array_jpg=array_diff(scandir($url.$directory[3]), array('..', '.'));
+  return($array_jpg);
+  }
+
+  function typeFile($url){
+    $directory = array_diff(scandir($url), array('..', '.'));
+    $imagen=$directory[3];
+    return($imagen);
+  }
+
+  function presentDate($date)
+  {
+      return Carbon::parse($date)->format('M d, Y');
+  }
+
+  function setActiveCategory($category, $output = 'active')
+  {
+      return request()->category == $category ? $output : '';
+  }
+
+  function productImage($path)
+  {
+      return $path && file_exists('storage/'.$path) ? asset('storage/'.$path) : asset('img/not-found.jpg');
+  }
+
+  function getshipingNumbers()
+  {
+      foreach (Cart::content() as $item){
+          $price =  $item->options->shiping;
+      }
+      if (isset($price)) {
+         return $price;
+      }
+
+  }
+
+  function getPrintingsides($prttime)
+  {
+     switch ($prttime) {
+         case '4/0':
+             return 'Front Only';
+             break;
+         case '4/4':
+             return 'Front and Back';
+             break;
+         default:
+             'N/A';
+             break;
+     }
+  }
+
+  function getPrintingTime($prttime)
+  {
       $method = 'GET';
       $separator ='?';
       $json = '';
-      $enponit='https://api.4over.com/printproducts/products/'.$UUID.'/baseprices';
-      $enponitoption='https://api.4over.com/printproducts/products/'.$UUID.'/optiongroups';
-      $result2 = json_decode(call4overcurl($enponitoption,$method,$separator,$json), true);
-
-      foreach ($result2['entities'] as $value) {
-        if ($value['product_option_group_name'] == 'Turn Around Time') {
-           $TurnAroundTimeoptions = $value['options'];
-          }
-       }
-      $result = json_decode(call4overcurl($enponit,$method,$separator,$json), true);
+      $uri='https://api.4over.com/printproducts/optiongroups/f80e8179-b264-42ce-9f80-bb258a09a1fe/options';
+      $result = json_decode(call4overcurl($uri,$method,$separator,$json), true);
       foreach ($result['entities'] as $value) {
-         if ($runsize ==$value['runsize'] && $colorspec ==$value['colorspec']) {
-          $firtprice = $value['product_baseprice'];
+          if ($value['option_name'] == $prttime) {
+             return $value['option_description'];
+            }
          }
-   }
-   foreach ($TurnAroundTimeoptions as $value) {
-    if ($runsize ==$value['runsize'] && $colorspec == $value['colorspec'] && $TurnAroundTime == $value['option_name']) {
-        $result3= json_decode(call4overcurl($value['option_prices'],$method,$separator,$json), true)['entities'];
-        if (isset($result3[0]['price'])) {
-            $secondprice = $result3[0]['price'];
-        }
-    }
-    if (isset($secondprice)) {
-        $sumprice = $firtprice+$secondprice;
-          return  number_format($sumprice, 2);
-    }else {
-           return number_format($firtprice, 2);
-    }
+  }
 
-    }
+  function getNumbers()
+  {
+      //tax for zip funtion
+      $cookie_name="Zipcode";
+      if(!isset($_COOKIE[$cookie_name])) {
+          $tax = 0 / 100;
+     } else {
+      $tax = printingtax($_COOKIE[$cookie_name]) / 100;
+     }
+      $discount = session()->get('coupon')['discount'] ?? 0;
+      $code = session()->get('coupon')['name'] ?? null;
+      $newSubtotal = (Cart::subtotal() - $discount);
+      $shiping = getshipingNumbers();
+      if ($newSubtotal < 0) {
+          $newSubtotal = 0;
+      }
+      $newTax = $newSubtotal * $tax;
+      $newTotal = ($newSubtotal * (1 + $tax)) + getshipingNumbers();
 
-}
-function getStockLevel($quantity)
-{
-    if ($quantity > setting('site.stock_threshold', 5)) {
-        $stockLevel = '<div class="badge badge-success">In Stock</div>';
-    } elseif ($quantity <= setting('site.stock_threshold', 5) && $quantity > 0) {
-        $stockLevel = '<div class="badge badge-warning">Low Stock</div>';
-    } else {
-        $stockLevel = '<div class="badge badge-danger">Not available</div>';
-    }
-    return $stockLevel;
-}
+      return collect([
+          'tax' => $tax,
+          'discount' => $discount,
+          'code' => $code,
+          'newSubtotal' => $newSubtotal,
+          'newTax' => $newTax,
+          'newTotal' => $newTotal,
+          'shiping' => $shiping,
+      ]);
+  }
+  function get4overprices($UUID,$runsize,$colorspec,$TurnAroundTime)
+  {
+        $method = 'GET';
+        $separator ='?';
+        $json = '';
+        $enponit='https://api.4over.com/printproducts/products/'.$UUID.'/baseprices';
+        $enponitoption='https://api.4over.com/printproducts/products/'.$UUID.'/optiongroups';
+        $result2 = json_decode(call4overcurl($enponitoption,$method,$separator,$json), true);
 
-function call4overcurl($uri,$method,$separator,$json) {
-    $public_key = 'printinglab';
-    $private_key = '4HT62RVQ';
-    if ($method == 'GET' || $method == 'DELETE') {
-    $apiPath=$uri.$separator.'apikey='.$public_key.'&signature='.hash_hmac('sha256', $method, hash('sha256', $private_key)).'&max=1000';
+        foreach ($result2['entities'] as $value) {
+          if ($value['product_option_group_name'] == 'Turn Around Time') {
+             $TurnAroundTimeoptions = $value['options'];
+            }
+         }
+        $result = json_decode(call4overcurl($enponit,$method,$separator,$json), true);
+        foreach ($result['entities'] as $value) {
+           if ($runsize ==$value['runsize'] && $colorspec ==$value['colorspec']) {
+            $firtprice = $value['product_baseprice'];
+           }
+     }
+     foreach ($TurnAroundTimeoptions as $value) {
+      if ($runsize ==$value['runsize'] && $colorspec == $value['colorspec'] && $TurnAroundTime == $value['option_name']) {
+          $result3= json_decode(call4overcurl($value['option_prices'],$method,$separator,$json), true)['entities'];
+          if (isset($result3[0]['price'])) {
+              $secondprice = $result3[0]['price'];
+          }
+      }
+      if (isset($secondprice)) {
+          $sumprice = $firtprice+$secondprice;
+            return  number_format($sumprice, 2);
+      }else {
+             return number_format($firtprice, 2);
+      }
+
+      }
+
+  }
+  function getStockLevel($quantity)
+  {
+      if ($quantity > setting('site.stock_threshold', 5)) {
+          $stockLevel = '<div class="badge badge-success">In Stock</div>';
+      } elseif ($quantity <= setting('site.stock_threshold', 5) && $quantity > 0) {
+          $stockLevel = '<div class="badge badge-warning">Low Stock</div>';
+      } else {
+          $stockLevel = '<div class="badge badge-danger">Not available</div>';
+      }
+      return $stockLevel;
+  }
+
+  function call4overcurl($uri,$method,$separator,$json) {
+      $public_key = 'printinglab';
+      $private_key = '4HT62RVQ';
+      if ($method == 'GET' || $method == 'DELETE') {
+      $apiPath=$uri.$separator.'apikey='.$public_key.'&signature='.hash_hmac('sha256', $method, hash('sha256', $private_key)).'&max=1000';
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$apiPath);
+      $args = array(
+        'method' => $method,
+        'timeout' => 20
+      );
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $args);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+      curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+      $result = curl_exec($ch);
+      curl_close($ch);
+      return $result;
+      }
+      if ($method == 'POST'){
+    $apiPath=$uri.$separator.'apikey='.$public_key.'&signature='.hash_hmac('sha256', $method, hash('sha256', $private_key)).'&max=500';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$apiPath);
-    $args = array(
-      'method' => $method,
-      'timeout' => 20
-    );
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $args);
+    curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
     $result = curl_exec($ch);
     curl_close($ch);
     return $result;
+      }
     }
-    if ($method == 'POST'){
-  $apiPath=$uri.$separator.'apikey='.$public_key.'&signature='.hash_hmac('sha256', $method, hash('sha256', $private_key)).'&max=500';
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL,$apiPath);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-  $result = curl_exec($ch);
-  curl_close($ch);
-  return $result;
-    }
-  }
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,10 +1,10 @@
 <header>
 
   <div class="barsignage">
-    <img src="/img/signage.svg" alt="">
+    
     <span>ARE YOU LOOKING FOR SIGNAGE, VEHICLE GRAPHICS OR TRADE SHOWS? <a target="_blank" href="http://signslab.com/">CLICK HERE</a>
     </span>
-    <img src="/img/signage.svg" alt="">
+
   </div>
   <div class="haderPc">
     <div class="container">
@@ -38,19 +38,11 @@
                     <div class="col-md-6">
                       <h6><strong>CUSTOM APPAREL</strong></h6>
                       <ul class="list_dropdown">
-                        <li><a href="#">Short Sleeve T-shirts</a></li>
-                        <li><a href="#">Long Sleeve T-shirts</a></li>
-                        <li><a href="#">Women</a></li>
-                        <li><a href="#">Hoodies</a></li>
-                        <li><a href="#">Sweatshirts</a></li>
-                        <li><a href="#">Activewear</a></li>
-                        <li><a href="#">Polos</a></li>
-                        <li><a href="#">Jackets</a></li>
+                        <li><a href="contact-us">Coming soon...</a></li>
                       </ul>
                       <h6 style="margin-top: 5%;"><strong>LARGE FORMAT</strong></h6>
                       <ul class="list_dropdown">
-                        <li><a href="#">Banners</a></li>
-                        <li><a href="#">Vinyl Graphics</a></li>
+                        <li><a href="contact-us">Coming soon...</a></li>
                       </ul>
                     </div>
                     <div class="col-md-6 img_MenuDrop">
@@ -78,7 +70,6 @@
       </div>
     </div>
   </div>
-
 
   <div class="headerMb">
     <nav class="navbar navbar-default">
@@ -166,14 +157,6 @@
                 </li>
                 @endforeach --}}
 
-
-
-
-
-
-
-
-
               </div>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -194,21 +177,13 @@
                 <li class="dropdown center">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CUSTOM APPAREL<span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Short Sleeve T-shirts</a></li>
-                    <li><a href="#">Long Sleeve T-shirts</a></li>
-                    <li><a href="#">Women</a></li>
-                    <li><a href="#">Hoodies</a></li>
-                    <li><a href="#">Sweatshirts</a></li>
-                    <li><a href="#">Activewear</a></li>
-                    <li><a href="#">Polos</a></li>
-                    <li><a href="#">Jackets</a></li>
+                    <li><a href="contact-us">Coming soon...</a></li>
                   </ul>
                 </li>
                 <li class="dropdown center">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">LARGE FORMAT<span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Banners</a></li>
-                    <li><a href="#">Vinyl Graphics</a></li>
+                    <li><a href="contact-us">Coming soon...</a></li>
                   </ul>
                 </li>
               </ul>
@@ -246,19 +221,11 @@
                         <div class="col-md-6">
                           <h6><strong>CUSTOM APPAREL</strong></h6>
                           <ul class="list_dropdown">
-                            <li><a href="#">Short Sleeve T-shirts</a></li>
-                            <li><a href="#">Long Sleeve T-shirts</a></li>
-                            <li><a href="#">Women</a></li>
-                            <li><a href="#">Hoodies</a></li>
-                            <li><a href="#">Sweatshirts</a></li>
-                            <li><a href="#">Activewear</a></li>
-                            <li><a href="#">Polos</a></li>
-                            <li><a href="#">Jackets</a></li>
+                            <li><a href="contact-us">Coming soon...</a></li>
                           </ul>
                           <h6 style="margin-top: 5%;"><strong>LARGE FORMAT</strong></h6>
                           <ul class="list_dropdown">
-                            <li><a href="#">Banners</a></li>
-                            <li><a href="#">Vinyl Graphics</a></li>
+                            <li><a href="contact-us">Coming soon...</a></li>
                           </ul>
                         </div>
                         <div class="col-md-6 img_MenuDrop">
